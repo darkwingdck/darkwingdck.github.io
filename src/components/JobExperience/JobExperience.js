0875ea59c703a7f4b59dd9e4ba5ce103ce70e45d
darkwingdck <dzakharov3819@gmail.com>
2023-11-15 20:38:29 +0300

    feat: add headings

--- src/components/JobExperience/JobExperience.js
+++ src/components/JobExperience/JobExperience.js
@@ -1,8 +1,10 @@
 import './JobExperience.css';
+import Heading from '../Ui/Heading/Heading';
 
 export default function JobExperience() {
   return (
     <div className="job-experience">
+      <Heading title='Job experience' />
       <h1>JobExperience</h1>
     </div>
   );
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,8 +1,10 @@
 import './Projects.css';
+import Heading from '../Ui/Heading/Heading';
 
 export default function Projects() {
   return (
     <div className="projects">
+      <Heading title='Projects' />
       <h1>Projects</h1>
     </div>
   );
