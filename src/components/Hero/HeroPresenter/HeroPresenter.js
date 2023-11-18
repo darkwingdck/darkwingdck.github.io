270fbebdd5dc7d6f7b234ec2c334e4947a307806
darkwingdck <dzakharov3819@gmail.com>
2023-11-18 21:56:34 +0300

    feat: presenter init animation

--- src/components/Hero/HeroPresenter/HeroPresenter.js
+++ src/components/Hero/HeroPresenter/HeroPresenter.js
@@ -3,40 +3,40 @@ import './HeroPresenter.css';
 export default function HeroPresenter() {
   return (
     <div className="hero-presenter">
-      <div className="block block_1">
+      <div className="block">
         <div className="block-inner block-inner_color-secondary block-inner_shape-quarter-bottom-right" />
       </div>
-      <div className="block block_2">
+      <div className="block">
         <div className="block-inner block-inner_color-primary block-inner_shape-quarter-bottom-right" />
       </div>
-      <div className="block block_3">
+      <div className="block">
         <div className="block-inner block-inner_color-secondary block-inner_shape-quarter-top-right" />
       </div>
-      <div className="block block_4">
+      <div className="block">
         <div className="block-inner block-inner_color-secondary block-inner_shape-quarter-top-left" />
       </div>
-      <div className="block block_5">
+      <div className="block">
         <div className="block-inner block-inner_color-accent-variant" />
       </div>
-      <div className="block block_6">
+      <div className="block">
         <div className="block-inner block-inner_color-accent block-inner_shape-quarter-bottom-left" />
       </div>
-      <div className="block block_7">
+      <div className="block">
         <div className="block-inner block-inner_color-secondary block-inner_shape-quarter-top-right" />
       </div>
-      <div className="block block_8">
+      <div className="block">
         <div className="block-inner block-inner_color-accent block-inner_shape-quarter-bottom-left" />
       </div>
-      <div className="block block_9">
+      <div className="block">
         <div className="block-inner block-inner_color-accent block-inner_shape-quarter-bottom-right" />
       </div>
-      <div className="block block_10">
+      <div className="block">
         <div className="block-inner block-inner_color-accent-variant block-inner_shape-quarter-bottom-right" />
       </div>
-      <div className="block block_11">
+      <div className="block">
         <div className="block-inner block-inner_color-secondary" />
       </div>
-      <div className="block block_12">
+      <div className="block">
         <div className="block-inner block-inner_color-primary block-inner_shape-circle" />
       </div>
       <div className="hero-presenter__image"></div>
