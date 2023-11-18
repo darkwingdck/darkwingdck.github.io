36a4cd902b144e36a460a4b447778c2ef3cc8559
darkwingdck <dzakharov3819@gmail.com>
2023-11-18 22:09:05 +0300

    feat: init fade in animation

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,7 +4,7 @@ import HeroPresenter from './HeroPresenter/HeroPresenter';
 export default function Hero() {
   return (
     <div className="hero">
-      <div className="hero-text">
+      <div className="hero-text animated">
         <div className="hero-text__title">Daniil Zakharov</div>
         <div className="hero-text__subtitle">Front-end dev</div>
       </div>
