99a5ce385c8f080bdf761f3c13cbfa9713274e50
darkwingdck <dzakharov3819@gmail.com>
2023-11-18 21:08:46 +0300

    feat: logo on click handler

--- src/components/Header/HeaderLogo/HeaderLogo.js
+++ src/components/Header/HeaderLogo/HeaderLogo.js
@@ -1,8 +1,15 @@
 import './HeaderLogo.css';
 
 export default function HeaderLogo() {
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
   return (
-    <div className="header-logo">
+    <div className="header-logo" onClick={scrollToTop}>
       DARKWGINDCK
     </div>
   );
