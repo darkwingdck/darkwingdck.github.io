cd26423f331e23fc6c272153381201df48f7ffaf
darkwingdck <dzakharov3819@gmail.com>
2023-11-18 18:09:21 +0300

    feat: header nav styles

--- src/components/Header/HeaderNav/HeaderNav.js
+++ src/components/Header/HeaderNav/HeaderNav.js
@@ -1,11 +1,23 @@
+import { useState } from 'react';
 import './HeaderNav.css';
 
 export default function HeaderNav() {
+  const [visible, setVisible] = useState(false);
+
+  const toggleNav = () => {
+    setVisible((old) => !old);
+  };
+
   return (
-    <nav className="header-nav">
-      <div className="header-nav__item">ABOUT ME</div>
-      <div className="header-nav__item">WORK</div>
-      <div className="header-nav__item">PROJECTS</div>
-    </nav>
+    <>
+      <nav className={`header-nav ${visible ? 'header-nav_opened' : ''}`}>
+        <div className="header-nav__item">ABOUT ME</div>
+        <div className="header-nav__item">WORK</div>
+        <div className="header-nav__item">PROJECTS</div>
+      </nav>
+      <div className="header-hamburger" onClick={toggleNav}>
+        {visible ? '×' : '≡'}
+      </div>
+    </>
   );
 };
